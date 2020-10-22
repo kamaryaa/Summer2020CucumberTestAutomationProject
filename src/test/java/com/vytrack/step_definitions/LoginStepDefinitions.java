68cc9ffe456b3fbdb5ff95e304860db6e1515ed5
kamaryaa <a1534193646@Gmail.com>
2020-10-22 11:09:31 -0400

    added CucumberRunner and LoginStepDefinitions classes

---
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,28 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinitions {
+
+    @Given("user is on the landing")
+    public void user_is_on_the_landing() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user logs in")
+    public void user_logs_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user should see dashboard page")
+    public void user_should_see_dashboard_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
