4df6352b9dbed6856a2a0c2d9ea81aa55a8f1023
kamaryaa <a1534193646@Gmail.com>
2020-10-23 11:17:44 -0400

    added Basepage.java class

---
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -0,0 +1,20 @@
+package com.vytrack.pages;
+
+import com.vytrack.utils.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+
+
+
+
+
+
+}
