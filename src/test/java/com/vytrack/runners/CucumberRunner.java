afb560aa31e0c5b06a14b0ca314dc00b0df86833
kamaryaa <a1534193646@Gmail.com>
2020-10-25 16:50:08 -0400

    added CreateCar.feature...

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,
-        tags = "@Login",
+        dryRun = true,
+        tags = "@add_car",
         publish = true
 
 )
---
+++ src/test/java/com/vytrack/step_definitions/CreateCarStepDefinitions.java
@@ -0,0 +1,16 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class CreateCarStepDefinitions {
+    @Given("user clicks on create car button")
+    public void user_clicks_on_create_car_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user adds new vehicle information")
+    public void user_adds_new_vehicle_information(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+}
